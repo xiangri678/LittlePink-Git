86c06e7f54da5e7483cc64f8f4cb47510490fcd9
Chloe Lauren <xiangri678@outlook.com>
2024-12-08 12:13:26 +0800

    Initial Commit

---
+++ LittlePink-Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink-Git
+//
+//  Created by Chloe Lauren on 12/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
