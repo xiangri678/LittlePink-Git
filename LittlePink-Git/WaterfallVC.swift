10b89bc6f593ff6b8c38b67a388343b0db2907ee
Chloe Lauren <xiangri678@outlook.com>
2024-12-14 20:17:56 +0800

    Compeleted Lesson 37

---
+++ LittlePink-Git/WaterfallVC.swift
@@ -0,0 +1,8 @@
+//
+//  WaterfallVC.swift
+//  LittlePink-Git
+//
+//  Created by Chloe Lauren on 12/14/24.
+//
+
+import Foundation
---
+++ VCs/WaterfallCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  LittlePink-Git
+//
+//  Created by Chloe Lauren on 12/14/24.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
